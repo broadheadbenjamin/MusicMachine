1c94ebfea089029ea6844877a00895ee40046977
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-07-16 16:48:12 -0600

    Added a button to simulate a download.

--- app/src/main/java/com/ge/musicmachine/MainActivity.java
+++ app/src/main/java/com/ge/musicmachine/MainActivity.java
@@ -2,12 +2,43 @@ package com.ge.musicmachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button mDownloadButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDownloadButton = findViewById(R.id.downloadButton);
+
+        mDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Downloading...", Toast.LENGTH_SHORT).show();
+                downloadSong();
+            }
+        });
+    }
+
+    private void downloadSong() {
+        long endTime = System.currentTimeMillis() + 10*1000;
+
+        while (System.currentTimeMillis() < endTime) {
+            try {
+                Thread.sleep(1*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Toast.makeText(this, "Song Downloaded", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Song downloaded.");
     }
 }
